336eee2b28f25dcb56140541fd988268128bf489
Michał Stolarz <mstolarz@puma.robotyka.ia.pw.edu.pl>
2018-10-30 13:44:33 +0100

    turning to beer

--- scripts/mini_projekt_1.py
+++ scripts/mini_projekt_1.py
@@ -181,6 +181,17 @@ def mapBuilding():
      q_dest = (0, 0)
      moveHead(q_dest)
 
+def findPlanExecute():
+     #velma.waitForInit()
+     T_B_Jar = velma.getTf("B", "beer") #roslaunch rcprg_gazebo_utils gazebo_publish_ros_tf_object.launch link_name:=beer::link frame_id:=beer
+   
+     z = T_B_Jar.p[2]#z
+     y = T_B_Jar.p[1]#y
+     x = T_B_Jar.p[0]#x
+
+     theta1 = math.atan2(y,x)#liczenie kata 
+
+     q_map_change['torso_0_joint'] = theta1
  
 if __name__ == "__main__":
      # starting position
@@ -197,6 +208,12 @@ if __name__ == "__main__":
 
      # left position
      q_map_left = {'torso_0_joint':1.55, 'right_arm_0_joint':-0.3, 'right_arm_1_joint':-1.8,
+         'right_arm_2_joint':1.25, 'right_arm_3_joint':0.85, 'right_arm_4_joint':0, 'right_arm_5_joint':-0.5,
+         'right_arm_6_joint':0, 'left_arm_0_joint':0.3, 'left_arm_1_joint':1.8, 'left_arm_2_joint':-1.25,
+         'left_arm_3_joint':-0.85, 'left_arm_4_joint':0, 'left_arm_5_joint':0.5, 'left_arm_6_joint':0 }
+
+      # changing dictionary
+     q_map_change = {'torso_0_joint':0, 'right_arm_0_joint':-0.3, 'right_arm_1_joint':-1.8,
          'right_arm_2_joint':1.25, 'right_arm_3_joint':0.85, 'right_arm_4_joint':0, 'right_arm_5_joint':-0.5,
          'right_arm_6_joint':0, 'left_arm_0_joint':0.3, 'left_arm_1_joint':1.8, 'left_arm_2_joint':-1.25,
          'left_arm_3_joint':-0.85, 'left_arm_4_joint':0, 'left_arm_5_joint':0.5, 'left_arm_6_joint':0 }
@@ -221,6 +238,39 @@ if __name__ == "__main__":
  
      rospy.sleep(0.5)
  
+     print "waiting for Planner init..."
+     p = Planner(velma.maxJointTrajLen())
+     if not p.waitForInit():
+         print "could not initialize PLanner"
+         exitError(2)
+     print "Planner init ok"
+
+     # define a function for frequently used routine in this test
+     def planAndExecute(q_dest):
+         print "Planning motion to the goal position using set of all joints..."
+         print "Moving to valid position, using planned trajectory."
+         goal_constraint = qMapToConstraints(q_dest, 0.01, group=velma.getJointGroup("impedance_joints"))
+         for i in range(20):
+             rospy.sleep(0.5)
+             js = velma.getLastJointState()
+             print "Planning (try", i, ")..."
+             traj = p.plan(js[1], [goal_constraint], "impedance_joints", max_velocity_scaling_factor=0.15, planner_id="RRTConnect")
+             if traj == None:
+                 continue
+             print "Executing trajectory..."
+             if not velma.moveJointTraj(traj, start_time=0.5):
+                 exitError(5)
+             if velma.waitForJoint() == 0:
+                 break
+             else:
+                 print "The trajectory could not be completed, retrying..."
+                 continue
+         rospy.sleep(0.5)
+         js = velma.getLastJointState()
+         if not isConfigurationClose(q_dest, js[1]):
+             exitError(6)
+
+
      diag = velma.getCoreCsDiag()
      if not diag.motorsReady():
          print "Motors must be homed and ready to use for this test."
@@ -235,27 +285,19 @@ if __name__ == "__main__":
  
      #initVelma();
  
-     # mapBuilding(); Odkrywanie otoczenia poprzez rozgladanie sie i obracanie
+     #mapBuilding(); Odkrywanie otoczenia poprzez rozgladanie sie i obracanie
+
 
      # Uwzglednianie mapy
-     print "waiting for Planner init..."
-     p = Planner(velma.maxJointTrajLen())
-     if not p.waitForInit():
-         print "could not initialize PLanner"
-         exitError(2)
-     print "Planner init ok"
      oml = OctomapListener("/octomap_binary")
      rospy.sleep(1.0)
      octomap = oml.getOctomap(timeout_s=5.0)
      p.processWorld(octomap)
 
-     #velma.waitForInit()
-     T_B_Jar = velma.getTf("B", "beer") #roslaunch rcprg_gazebo_utils gazebo_publish_ros_tf_object.launch link_name:=beer::link frame_id:=beer
-   
-     print T_B_Jar.p[2]
-     print T_B_Jar.p[1]
-     print T_B_Jar.p[0]
+     findPlanExecute(); #znajdowanie puszki i obliczenie kata
+     planAndExecute(q_map_change); #obracanie do puszki
 
+     
 
 
     # if not velma.moveCartImpRight([T_B_Jar], [0.1], [PyKDL.Frame()], [0.1], None, None, PyKDL.Wrench(PyKDL.Vector(5,5,5), PyKDL.Vector(5,5,5)), start_time=0.5):
